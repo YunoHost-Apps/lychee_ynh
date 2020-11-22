00c1e09f1b03c1259c82430324d3cdd2ed14ff66
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2020-11-22 18:20:24 +0100

    Storage (#5)
    - Set the media storage to /home.yunohost.app/lychee
    
    Co-authored-by: tituspijean <tituspijean@outlook.com>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,9 @@ YNH_PHP_VERSION=7.3
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-zip"
 
+# needed for raw upload and video playback
+pkg_dependencies="ufraw-batch ffmpeg"
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
@@ -70,49 +73,4 @@ ynh_install_composer () {
     # update dependencies to create composer.lock
     ynh_composer_exec --phpversion="${phpversion}" --workdir="$workdir" --commands="install --no-dev $install_args" \
         || ynh_die "Unable to update core dependencies with Composer."
-}
-
-
-
-
-# Install or update the main directory yunohost.multimedia
-#
-# usage: ynh_multimedia_build_main_dir
-ynh_multimedia_build_main_dir () {
-        local ynh_media_release="v1.2"
-        local checksum="806a827ba1902d6911095602a9221181"
-
-        # Download yunohost.multimedia scripts
-        wget -nv https://github.com/YunoHost-Apps/yunohost.multimedia/archive/${ynh_media_release}.tar.gz 
-
-        # Check the control sum
-        echo "${checksum} ${ynh_media_release}.tar.gz" | md5sum -c --status \
-                || ynh_die "Corrupt source"
-
-    # Check if the package acl is installed. Or install it.
-    ynh_package_is_installed 'acl' \
-        || ynh_package_install acl
-
-        # Extract
-        mkdir yunohost.multimedia-master
-        tar -xf ${ynh_media_release}.tar.gz -C yunohost.multimedia-master --strip-components 1
-        ./yunohost.multimedia-master/script/ynh_media_build.sh
-}
-
-# Add a directory in yunohost.multimedia
-# This "directory" will be a symbolic link to a existing directory.
-#
-# usage: ynh_multimedia_addfolder "Source directory" "Destination directory"
-#
-# | arg: -s, --source_dir= - Source directory - The real directory which contains your medias.
-# | arg: -d, --dest_dir= - Destination directory - The name and the place of the symbolic link, relative to "/home/yunohost.multimedia"
-ynh_multimedia_addfolder () {
-    # Declare an array to define the options of this helper.
-    declare -Ar args_array=( [s]=source_dir= [d]=dest_dir= )
-    local source_dir
-    local dest_dir
-    # Manage arguments with getopts
-    ynh_handle_getopts_args "$@"
-
-    ./yunohost.multimedia-master/script/ynh_media_addfolder.sh --source="$source_dir" --dest="$dest_dir"
 }
