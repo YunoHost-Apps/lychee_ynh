72d1696dba097ac997b592566b47cd38ad326dbc
ericgaspar <junk.eg@free.fr>
2021-01-24 16:08:42 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,7 +12,7 @@ extra_php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick 
 # EXPERIMENTAL HELPERS
 #=================================================
 
-readonly YNH_DEFAULT_COMPOSER_VERSION=1.10.17
+readonly YNH_DEFAULT_COMPOSER_VERSION=2.0.8
 # Declare the actual composer version to use.
 # A packager willing to use another version of composer can override the variable into its _common.sh.
 YNH_COMPOSER_VERSION=${YNH_COMPOSER_VERSION:-$YNH_DEFAULT_COMPOSER_VERSION}
