4d7249793385b6a4ee493657a9843b9cbfb76392
ericgaspar <junk.eg@free.fr>
2021-01-24 15:24:25 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ YNH_PHP_VERSION=7.4
 extra_php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-zip"
 
 # needed for raw upload and video playback
-pkg_dependencies="ufraw-batch ffmpeg"
+pkg_dependencies="ufraw-batch ffmpeg"  #redis-server redis-tools
 
 #=================================================
 # EXPERIMENTAL HELPERS
