1cbe50b57b1a963bda5aa4638235546460078d45
ericgaspar <junk.eg@free.fr>
2022-03-29 16:18:03 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ YNH_PHP_VERSION="8.0"
 # Composer version
 YNH_COMPOSER_VERSION="2.1.3"
 
-pkg_dependencies="ffmpeg php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-intl"
+pkg_dependencies="ffmpeg php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-intl"
 
 # # needed for raw upload and video playback
 # # ufraw-batch is not supported in bullseye
