f9078b755f1d97ca563cd0f2957e89812260596b
ericgaspar <junk.eg@free.fr>
2022-05-16 15:59:08 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 YNH_PHP_VERSION="8.0"
 
 # Composer version
-YNH_COMPOSER_VERSION="2.1.3"
+YNH_COMPOSER_VERSION="2.3.5"
 
 pkg_dependencies="ffmpeg php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-intl"
 
