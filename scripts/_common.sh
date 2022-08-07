577b9b06352274e3aa5b7b43c24b62d1e9212d8c
yalh76 <yalh@yahoo.com>
2022-08-07 18:40:41 +0200

    Apply example_ynh (#43)
    * Auto-update README
    
    * Apply example_ynh
    
    * Auto-update README
    
    * fix missing datadir
    
    * Fix missing phpversion
    
    * Update upgrade
    
    * Update check_process
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,6 +9,7 @@ YNH_PHP_VERSION="8.0"
 # Composer version
 YNH_COMPOSER_VERSION="2.3.5"
 
+# dependencies used by the app
 pkg_dependencies="ffmpeg php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-intl"
 
 #=================================================
