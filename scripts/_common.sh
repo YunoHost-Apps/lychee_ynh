930ada66ff6c41c5c826249aedb29c560e0fc3b0
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-05 23:47:51 +0100

    4.7.0 (#50)
    * 4.6.2
    
    * Auto-update README
    
    * 4.7.0
    
    * Auto-update README
    
    * Fix PHP
    
    * Update upgrade
    
    * Update restore
    
    * Update install
    
    * Update install
    
    * Update PHP to 8.1
    
    * Update upgrade
    
    * Update install
    
    * Fix
    
    * Update ffprobe.patch
    
    * Update install
    
    * Update install
    
    * Update upgrade
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,13 +4,15 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="8.0"
+YNH_PHP_VERSION="8.1"
 
 # Composer version
 YNH_COMPOSER_VERSION="2.3.5"
 
 # dependencies used by the app
-pkg_dependencies="ffmpeg php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-intl"
+php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-intl"
+
+pkg_dependencies="ffmpeg $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
