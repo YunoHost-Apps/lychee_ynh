6a8f1f0122121f3f3c96166a227e46b5f4a9d64d
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-05 22:13:51 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,15 +4,8 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="8.1"
-
 # Composer version
 YNH_COMPOSER_VERSION="2.3.5"
-
-# dependencies used by the app
-#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-intl"
-
-#REMOVEME? pkg_dependencies="ffmpeg $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
