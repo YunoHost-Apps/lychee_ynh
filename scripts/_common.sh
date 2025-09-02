6822d131b7375d7615b25e07a5dfa87792e7f417
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2025-09-02 09:42:37 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,3 +5,10 @@
 #=================================================
 
 composer_version="2.7.7"
+
+temp_fix() {
+
+ynh_replace --match="$this->base_url = url(config('app.dir_url') . '/');" --replace="$this->base_url = url('/');" --file="$install_dir/app/View/Components/Meta.php"
+ynh_replace --match="'asset_url' => env('ASSET_URL')," --replace="'asset_url' => null," --file="config/app.php"
+
+}
